0e0e91ffb17b8901785e315c885a89ec0e429296
Anthony Pandiani <110365300+apandiani@users.noreply.github.com>
2023-10-27 10:44:31 +0200

    Update main.py

--- main.py
+++ main.py
@@ -13,14 +13,14 @@ async def clock():
 run_clock = asyncio.ensure_future(clock())
 
 
-def get_timestamp():
-    timestamp_csv = 'timestamp.csv'
-    with open(timestamp_csv) as csvfile:
-        reader = csv.reader(csvfile)
-        time_list = [row for row in reader]
-        last_log = time_list[-1][0]
-        time_date = last_log[:-6]
-        Element("logged_time").write(time_date)
-        # print(time_date)
-timestamp = get_timestamp()
+# def get_timestamp():
+#     timestamp_csv = 'timestamp.csv'
+#     with open(timestamp_csv) as csvfile:
+#         reader = csv.reader(csvfile)
+#         time_list = [row for row in reader]
+#         last_log = time_list[-1][0]
+#         time_date = last_log[:-6]
+#         Element("logged_time").write(time_date)
+#         # print(time_date)
+# timestamp = get_timestamp()
 
