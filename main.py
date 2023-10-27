7f5abcecca16e5ad149b1e730ec6e8c0b0551162
Anthony Pandiani <110365300+apandiani@users.noreply.github.com>
2023-10-27 10:43:44 +0200

    Add files via upload

---
+++ main.py
@@ -0,0 +1,26 @@
+from datetime import datetime as dt
+import asyncio
+import csv
+
+
+async def clock():
+    while True:
+        now = dt.now()
+        time = now.strftime("%Y-%m-%d %H:%M")
+        # print(time)
+        Element("time_output").write(time)
+        await asyncio.sleep(2)
+run_clock = asyncio.ensure_future(clock())
+
+
+def get_timestamp():
+    timestamp_csv = 'timestamp.csv'
+    with open(timestamp_csv) as csvfile:
+        reader = csv.reader(csvfile)
+        time_list = [row for row in reader]
+        last_log = time_list[-1][0]
+        time_date = last_log[:-6]
+        Element("logged_time").write(time_date)
+        # print(time_date)
+timestamp = get_timestamp()
+
