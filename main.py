debeae94753b77890db08be7f7da5611fc2953d6
Anthony Pandiani <110365300+apandiani@users.noreply.github.com>
2023-11-07 10:16:39 +0100

    Update main.py

--- main.py
+++ main.py
@@ -1,13 +1,14 @@
 from datetime import datetime as dt
 import asyncio
-# import csv
+import csv
+from pyscript import document
 
 
 async def clock():
     while True:
         now = dt.now()
         time = now.strftime("%Y-%m-%d %H:%M")
-        # print(time)
-        Element("time_output").write(time)
+        output_div = document.querySelector("#time_output")
+        output_div.innerText = time
         await asyncio.sleep(2)
 run_clock = asyncio.ensure_future(clock())
